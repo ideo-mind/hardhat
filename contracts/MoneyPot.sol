d97826e49147380dae8ac2c8d31d42195beed3ad
Hiro <laciferin@gmail.com>
2025-10-26 18:17:33 +0530

    lower: difficulty mod

--- contracts/MoneyPot.sol
+++ contracts/MoneyPot.sol
@@ -20,7 +20,7 @@ contract MoneyPot is MoneyPotToken, MoneyPotPyth {
     using SafeERC20 for IERC20Metadata;
 
     // Constants
-    uint256 public constant DIFFICULTY_MOD = 9;
+    uint256 public constant DIFFICULTY_MOD = 2;
     uint256 public constant HUNTER_SHARE_PERCENT = 90;
     uint256 public constant CREATOR_ENTRY_FEE_SHARE_PERCENT = 50;
     uint256 public constant MIN_FEE = 1 wei;
